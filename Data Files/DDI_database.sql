4ccffecef1a8f9b3006b23430892f05a5e3f5b42
Wren <warrenandros@gmail.com>
2025-04-06 18:18:52 -0400

    DDI db in SQL complete

--- Data Files/DDI_database.sql
+++ Data Files/DDI_database.sql
@@ -1,17 +1,29 @@
-create database if not exists DDI;
+
+create user if not exists 'admin'@'localhost' identified by 'group7';
+drop database if exists DDI;
+create database DDI;
+grant all privileges on *.* to 'admin'@'localhost';
 use DDI;
-create table if not exists interactions (
+inputs
+create table interactions (
 	int_code int not null,
-    int_reduced_code int,
     int_desc mediumtext not null,
     primary key (int_code)
 );
 
-create table if not exists inputs (
+create table y_codes (
+    int_reduced_code int not null,
+    int_code int not null,
+    primary key (int_reduced_code),
+    foreign key (int_code) references interactions(int_code)
+);
+
+create table inputs (
     inp_num int not null,
     molecule1 varchar(2800),
     molecule2 varchar(2800),
     result int,
+    result_reduced bool,
     primary key (inp_num),
-    foreign key (result) references interactions(int_code)
+    foreign key (result) references y_codes(int_reduced_code)
 );
